f5e18cd7fdeab62322258118679e111be9eff124
halilergul <halilergul@hotmail.com.tr>
2017-05-26 15:28:10 +0300

    halisaha_v0.6

--- src/config.js
+++ src/config.js
@@ -26,3 +26,14 @@ app.config(['$routeProvider', function($routeProvider, $scope) {
         });
 
 }]);
+
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
